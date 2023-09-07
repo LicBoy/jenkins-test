fe2278662cd9ab7a8f6bcdac568ad2d4b38d751c
LicBoy <ahmetvaleevr@gmail.com>
2023-09-07 12:56:01 +0600

    commit after rebase
    fix agent in jenkinsfile
    
    asd
    
    adsasd
    
    big test
    
    asda
    
    fix type
    
    sh to bat

--- run.py
+++ run.py
@@ -1,13 +1,15 @@
 import os
 
 # Access Jenkins parameters using environment variables
-param1 = os.environ.get('asdsadsa')
-param2 = os.environ.get('browsername')
-param3 = os.environ.get('long string')
+param1 = os.environ.get('NUM_THREADS')
+param2 = os.environ.get('BOOL_PARAM_1')
+param3 = os.environ.get('BOOL_PARAM_2')
+param4 = os.environ.get('FRUIT')
 
 # Now you can use these parameters in your script
 print("Parameter 1:", param1, type(param1))
 print("Parameter 2:", param2, type(param2))
 print("Parameter 3:", param3, type(param3))
+print("Parameter 4:", param4, type(param4))
 
 print(os.environ)
